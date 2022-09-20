0696162ec95dcc1d0150b757c76dbb3ac1f824f7
sangjun-Park1208 <77184523+sangjun-Park1208@users.noreply.github.com>
2022-09-20 22:56:50 +0900

    feat: Add Splash Activity(~ing)

---
+++ app/src/main/java/com/example/asapd/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.example.asapd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.asapd.ui.login.LoginActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
