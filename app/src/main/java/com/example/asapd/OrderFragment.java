bc14a4463a9194572b2616c025eb6b32b682f007
sangjun-Park1208 <77184523+sangjun-Park1208@users.noreply.github.com>
2022-09-24 21:41:45 +0900

    feat: Add SharedPreference in OrderFragment

--- app/src/main/java/com/example/asapd/OrderFragment.java
+++ app/src/main/java/com/example/asapd/OrderFragment.java
@@ -1,20 +1,31 @@
 package com.example.asapd;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentResultListener;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.ArrayList;
+import com.example.asapd.MemberRequest.MemberContactRequest;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import okhttp3.Headers;
+import okhttp3.internal.http2.Header;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -24,8 +35,8 @@ public class OrderFragment extends Fragment {
     private RecyclerView mRecyclerView;
     private RecyclerItemAdapter mRecyclerAdapter;
     private ArrayList<ItemData> mList;
-    private Button btn;
-
+    private Button btn_charge;
+    private EditText editTextTextPersonName;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -34,6 +45,9 @@ public class OrderFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private ArrayList<ItemData> mlist;
+
+    private SharedPreferences preferences;
 
     public OrderFragment(){
         // Required empty public constructor
@@ -72,7 +86,23 @@ public class OrderFragment extends Fragment {
         // Inflate the layout for this fragment
         ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_order_info, container, false);
 
-        btn = rootView.findViewById(R.id.btn_charge);
+        btn_charge = rootView.findViewById(R.id.btn_charge); // 주문하기
+        editTextTextPersonName = rootView.findViewById(R.id.editTextTextPersonName); // 상세 주소 입력
+
+        preferences = this.getActivity().getSharedPreferences("pref", Context.MODE_PRIVATE);
+        Log.d("TAG", preferences.getString("TOKEN", "NULL"));
+
+        btn_charge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String destination = editTextTextPersonName.getText().toString();
+                Map<String, Integer> items = new HashMap<>();
+                // items 정보 들고 오기
+
+                OrderRequest orderRequest = new OrderRequest(destination, items);
+                sendCode(orderRequest);
+            }
+        });
         mRecyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerView);
         mList = ItemData.createList(5);
         mRecyclerView.setHasFixedSize(true);
@@ -82,5 +112,31 @@ public class OrderFragment extends Fragment {
 
         return rootView;
 
+    }
+
+    private void sendCode(ItemsOrderRequest itemsOrderRequest){
+        RetrofitClient.getApiService().itemsOrder(itemsOrderRequest).enqueue(new Callback<BaseResponse>() {
+            @Override
+            public void onResponse(Call<BaseResponse> call, Response<BaseResponse> response) {
+                Headers headers = response.headers();
+                BaseResponse result = response.body();
+                if (result.getStatus() == 200) {
+                    Log.d("TAG", result.getMessage());
+
+                }
+                else if (result.getStatus() == 500) {
+                    Log.d("TAG", result.getMessage());
+                }
+                else{
+                    Log.d("TAG", result.getMessage());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<BaseResponse> call, Throwable t) {
+                Log.e("Send code Error", t.getMessage());
+                t.printStackTrace();
+            }
+        });
     }
 }
